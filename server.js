92235f6990dc1ef3f87c1eef84ddeae99d741fad
Alex McDonald <acm4219@gmail.com>
2020-10-08 21:12:56 -0500

    fixed some routing, though still figuring out how to get the server running

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
